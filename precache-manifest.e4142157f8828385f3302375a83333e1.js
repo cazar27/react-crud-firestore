235cbeab221cff7d38461fdb35cc590729e5a94c
Carlos Zamorano <carlos.zamorano@gus.global>
2021-01-24 00:11:23 +0100

    Updates

---
+++ precache-manifest.e4142157f8828385f3302375a83333e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a1845885f846b9ac845f48689076b3",
+    "url": "/react-firebase-crud/index.html"
+  },
+  {
+    "revision": "d537338d0881158d4146",
+    "url": "/react-firebase-crud/static/css/2.7301639b.chunk.css"
+  },
+  {
+    "revision": "eacb6591ca13241ba8d9",
+    "url": "/react-firebase-crud/static/css/main.884087ca.chunk.css"
+  },
+  {
+    "revision": "d537338d0881158d4146",
+    "url": "/react-firebase-crud/static/js/2.ed7a69f5.chunk.js"
+  },
+  {
+    "revision": "baf8e19b81885464d04568dc57ed0d6b",
+    "url": "/react-firebase-crud/static/js/2.ed7a69f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacb6591ca13241ba8d9",
+    "url": "/react-firebase-crud/static/js/main.48c0d361.chunk.js"
+  },
+  {
+    "revision": "34c1d525d137c8bc05a1",
+    "url": "/react-firebase-crud/static/js/runtime-main.7eacdd7f.js"
+  }
+]);
